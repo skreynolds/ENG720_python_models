6635667cee49dbb1d74b77c8e777f0e696ff1955
skreynolds <shane.k.reynolds@gmail.com>
2020-05-12 14:47:49 +0930

    Add: completed and untested two area model with segregated system and controller

--- 02_single_area_models/02_PI_control/04_single_area_model_PI_control_seg_iterative_model.py
+++ 02_single_area_models/02_PI_control/04_single_area_model_PI_control_seg_iterative_model.py
@@ -13,7 +13,6 @@ def del_p_L_func(t):
         del_p_L = 0.2
     return del_p_L
 
-
 # Setting up first order model for power system
 def int_power_system_sim(x_sys, t, control_sig, K_sg=1, T_sg=0.2,
                          K_t=1, T_t=0.5, K_gl=1, T_gl=10):
@@ -23,14 +22,15 @@ def int_power_system_sim(x_sys, t, control_sig, K_sg=1, T_sg=0.2,
     x_5_dot = (K_gl/T_gl)*x_sys[1] - (K_gl/T_gl)*del_p_L_func(t) - (0.8/T_gl)*x_sys[2]
     return x_3_dot, x_4_dot, x_5_dot
 
-
 # Setting up first order model for controller
-def int_control_system_sim(x_control, t, x_frequency, R=1/20, K_i=-7):
-    x_2_dot = K_i*x_frequency
+def int_control_system_sim(x_control_sys, t, frequency_sig, R=1/20, K_i=-7):
+    x_2_dot = K_i*frequency_sig
     return x_2_dot
 
 
-
+##################################################################
+# Main function that is executed when file called from terminal
+##################################################################
 def main():
     # Set model constants
     K_sg = 1
@@ -47,18 +47,19 @@ def main():
     t_max = 11 # max sim time (seconds)
 
     # Set variable initial conditions
-    x_2_0 = 0.0 # x_control[0]  (system control signal)
+    x_2_0 = 0.0 # x_control_sys  (system control signal)
     x_3_0 = 0.0 # x_sys[0]
     x_4_0 = 0.0 # x_sys[1]
-    x_5_0 = 0.0 # x_sys[2]      (system frequency)
+    x_5_0 = 0.0 # x_sys[2]       (system frequency)
 
     x_sys = (x_3_0, x_4_0, x_5_0)       # initialise system
-    x_2 = x_2_0                         # initialise integral control input
+    x_freq = x_5_0                      # initialise frequency signal
+    x_control_sys = x_2_0               # initialise integral control input
     x_control = x_2_0 - (1/R)*x_5_0     # initialise controller signal
 
     # initialise empty list to store simulation output
-    out_s = [x_5_0]     # frequency output
-    out_c = [x_2_0]    # controller output
+    out_s = [x_freq]     # frequency output
+    out_c = [x_control]    # controller output
 
     # initialise the time
     t = 0
@@ -71,9 +72,12 @@ def main():
 
         ##############################################################
         # step the system simulation forward in time
+        ##############################################################
+
         # arguement tuple
         arg_sys = (x_control, K_sg, T_sg, K_t, T_t, K_gl, T_gl)
-        #print(arg_sys)
+
+        # time step simulation
         x_sys_vals = integrate.odeint(int_power_system_sim,         # ode system
                                       x_sys,                        # initial cond
                                       np.array([t, t_step]),        # time step
@@ -82,29 +86,35 @@ def main():
 
         ##############################################################
         # step the control simulation forward in time
-        arg_control = (x_sys[2], R, K_i)
+        ##############################################################
+
+        # arguement tuple
+        arg_control = (x_freq, R, K_i)
+
+        # time step simulation
         x_control_vals = integrate.odeint(int_control_system_sim,   # ode system
-                                          x_2,                      # initial cond
+                                          x_control_sys,            # initial cond
                                           np.array([t, t_step]),    # time step
                                           args=arg_control)         # model args
         ##############################################################
 
-        x_3 = x_sys_vals[1, 0] # x_sys[0]
-        x_4 = x_sys_vals[1, 1] # x_sys[1]
-        x_5 = x_sys_vals[1, 2] # x_sys[2] (system frequency)
+        # save the new init values for the system
+        x_3 = x_sys_vals[1, 0]      # x_sys[0]
+        x_4 = x_sys_vals[1, 1]      # x_sys[1]
+        x_freq = x_sys_vals[1, 2]   # x_sys[2] (system frequency)
 
-        # create new x_sys
-        x_sys = (x_3, x_4, x_5)
+        # create new x_sys (i.e. initial conditions for next ode step)
+        x_sys = (x_3, x_4, x_freq)
 
-        # save the new init values
-        x_2 = x_control_vals[1, 0] # x_init[0]
+        # save the new init values for the controller
+        x_control_sys = x_control_vals[1, 0] # control output from integral block
 
         # create new x_control
-        x_control = x_2 - (1/R)*x_5
+        x_control = x_control_sys - (1/R)*x_freq
 
         # save the output
-        out_s.append(x_5)
-        out_c.append(x_2)
+        out_s.append(x_freq)
+        out_c.append(x_control)
 
         # set t to the next time step
         t = t_step
