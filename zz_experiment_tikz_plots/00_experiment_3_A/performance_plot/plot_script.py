32b59ac5ebd904cc3162ffdc560f5fcaac3315a4
skreynolds <shane.k.reynolds@gmail.com>
2020-06-13 10:07:05 +0930

    Add: plots for preliminary experiments undertaken

---
+++ zz_experiment_tikz_plots/00_experiment_3_A/performance_plot/plot_script.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# import required libraries
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+from tikzplotlib import save as tikz_save
+
+def main():
+
+	# Import the pickle results
+	with open('dump.pkl', 'rb') as f:
+		pi_results = pickle.load(f)
+
+	with open('ddpg_dump.pkl', 'rb') as f:
+		ddpg_results = pickle.load(f)
+
+
+	# Tikz plot the system response
+	plt.plot(pi_results[0], pi_results[1])
+	plt.plot(ddpg_results[0], ddpg_results[1])
+	tikz_save('frequency_response_1.tikz')
+	plt.close()
+
+	# Tikz plot the system response
+	plt.plot(pi_results[0], pi_results[2])
+	plt.plot(ddpg_results[0], ddpg_results[2])
+	tikz_save('frequency_response_2.tikz')
+	plt.close()
+
+	# Tikz plot the first control signal
+	plt.plot(pi_results[0], pi_results[3])
+	plt.plot(ddpg_results[0], [0.1*e for e in ddpg_results[3]])
+	tikz_save('control_signal_1.tikz')
+	plt.close()
+
+	# Tikz plot the second control signal
+	plt.plot(pi_results[0], pi_results[4])
+	plt.plot(ddpg_results[0], [0.1*e for e in ddpg_results[4]])
+	tikz_save('control_signal_2.tikz')
+	plt.close()
+
+
+if __name__ == '__main__':
+	main()
---
+++ zz_experiment_tikz_plots/01_experiment_4_B/reward_plot/plot_script.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# import required libraries
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+from scipy.ndimage.filters import uniform_filter1d
+from tikzplotlib import save as tikz_save
+
+def main():
+
+    # Import pickle resuts
+    with open('plot_200.pkl', 'rb') as f:
+        training_reward = pickle.load(f)
+
+    # Create the episode list
+    episode = list(range(1,201))
+
+    # Create the average list for plotting
+    avg_list = [np.mean(training_reward[:(i+1)]) for i in range(len(training_reward))]
+
+    # Smooth the average list
+    avg_list_smoothed = uniform_filter1d(avg_list, 1)
+
+    # Create the standard deviation list
+    stdev_list = [np.std(training_reward[:(i+1)]) for i in range(len(training_reward))]
+    stdev_list_smoothed = uniform_filter1d(stdev_list, 1)
+
+    # Plot the episodic raw reward
+    plt.plot(episode, training_reward)
+    tikz_save('raw_reward_plot.tikz')
+    plt.close()
+
+    # Plot the average reward for and standard deviation band
+    plt.plot(episode, avg_list)
+    plt.fill_between(episode,
+                     avg_list_smoothed + stdev_list_smoothed,
+                     avg_list_smoothed - stdev_list_smoothed,
+                     alpha=0.3,
+                     edgecolor="#ff7f0e")
+
+    tikz_save('average_reward_plot.tikz')
+    plt.close()
+
+
+
+if __name__ == '__main__':
+    main()
